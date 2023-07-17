231e700a2fe201bb17301cec5386d9718d73c8c9
smhrd <smhrd@220.93.229.148>
2023-07-17 16:06:56 +0900

    팀원3 네번째 커밋

--- GitTest/src/Ex01.java
+++ GitTest/src/Ex01.java
@@ -6,7 +6,11 @@ public class Ex01 {
 		System.out.println("첫번쨰 커밋");
 		System.out.println("팀원1 두번째 커밋");
 		System.out.println("팀원2 세번쨰 커밋");
+		System.out.println("팀원3 네번째 커밋");
 		System.out.println("팀원4 다섯번쨰 커밋");
+		
+	
+	
 	}
 
 }
